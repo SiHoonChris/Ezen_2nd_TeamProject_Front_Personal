59b22881752328ac7a514d01d4311c53730056da
SiHoonChris <chrisleegpock@gmail.com>
2023-04-04 00:08:19 +0900

    11th commit 04.03 (comments on commit 참고)

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 import MainComp from '../components/MainComp.vue'
 
 const routes = [
-  { path: '/', name: 'home', component: MainComp },
-  { path: '/login', name: 'login', component: () => import("../views/LoginView.vue") },
-  { path: '/categories/item/:cno', name: 'item', component: () => import("../views/ItemsView.vue") },
-  { path: '/product/search/:typed', name: 'search', component: () => import("../views/SearchItems.vue") },
-  { path: '/product/detail/:pno', name: 'product', component: () => import("../views/ProductDetail.vue") }
+  { path: '/',                      name: 'home',    component: MainComp                                   },
+  { path: '/login',                 name: 'login',   component: () => import("../views/LoginView.vue")     },
+  { path: '/categories/item/:cno',  name: 'item',    component: () => import("../views/ItemsView.vue")     },
+  { path: '/product/search/:typed', name: 'search',  component: () => import("../views/SearchItems.vue")   },
+  { path: '/product/detail/:pno',   name: 'product', component: () => import("../views/ProductDetail.vue") },
+  { path: '/cart',                  name: 'cart',    component: () => import("../views/Cart.vue")          }
 ]
 
 const router = createRouter({
